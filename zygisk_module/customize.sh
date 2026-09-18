70b282b5aae4cc4df7d4a5ac7ef35c4bf24d189e
kingmaker91317-ops <kingmaker91317@gmail.com>
2026-09-18 20:11:32 +0530

    Add Zygisk Magisk module configuration & files

---
+++ zygisk_module/customize.sh
@@ -0,0 +1,10 @@
+SKIPUNZIP=0
+
+ui_print "=========================================="
+ui_print "       THE LION CHEATS V1 ZYGISK          "
+ui_print "=========================================="
+ui_print "- Installing Zygisk Module..."
+ui_print "- Target Games: Free Fire & Free Fire MAX"
+ui_print "- Target Architectures: ARM64 & ARMv7"
+ui_print "- Status: Success!"
+ui_print "=========================================="
